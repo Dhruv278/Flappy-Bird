ca6255901091f48d7434a81a94a9f537d5df8842
Dhruv Gopani <dhruvgopani8@gmail.com>
2020-07-13 12:04:12 +0530

    'initial'

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get('/',(req,res)=>{
     res.status(200).render('main')
 })
 const port =process.env.PORT||5000;
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log("Serevr is running");
 })
